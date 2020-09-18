d370fe7fe7500fdbe3a64ade37054c746a3dd093
jaspertham <jaspertham98@gmail.com>
2020-09-19 01:45:49 +0800

    added stripe

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HSnXvHDf1mMeDzFNf2UClL8aQgBfWRkfCHwcMC5aljfiYMARNZbz7uzB09cZ7kuMkiK37xqGKZmNNZIrySeDTSm00m081yfLD"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -41,7 +48,12 @@ function App() {
             <Header />
             <Checkout />
           </Route>
-
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
           <Route path="/">
             <Header />
             <Home />
